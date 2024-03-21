c65830aba2115559696208fe7569e8f79bd90cf8
simonhodne <nomis.hodne@gmail.com>
2024-03-21 04:23:02 +0100

    Created framework for first task

---
+++ Program/GreetingFunction.cs
@@ -0,0 +1,10 @@
+﻿namespace Program;
+
+public static class GreetingFunction
+{
+    public static string GreetPerson(string name)
+    {
+        string output = "";
+        return output;
+    }
+}
---
+++ Program/MathFunctions.cs
@@ -0,0 +1,32 @@
+﻿namespace Program;
+
+public static class MathFunctions
+{
+    public static double ReturnSquare(double number)
+    {
+        double output = 0.0;
+        return output;
+    }
+
+    public static double ConvertInchesToMM(double number)
+    {
+        double output = 0.0;
+        return output;
+    }
+    public static double ReturnSquareRoot(double number)
+    {
+        double output = 0.0;
+        return output;
+    }
+    public static double ReturnCube(double number)
+    {
+        double output = 0.0;
+        return output;
+    }
+    public static double ReturnAreaOfCircle(double radius)
+    {
+        double output = 0.0;
+        return output;
+    }
+    
+}
