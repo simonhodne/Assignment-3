ec9164e18bf1f2f4ff13ba1d2b983d8b60ef494f
simonhodne <nomis.hodne@gmail.com>
2024-03-22 23:55:25 +0100

    FlattenArray GREEN Test

---
+++ Program/ArrayFlattener.cs
@@ -0,0 +1,38 @@
+﻿namespace Program;
+
+public static class ArrayFlattener
+{
+    static List<int> intList = new List<int>();
+    
+    public static int[] FlattenArray(object[] array)
+    {
+        Dig(array);
+        
+        int[] output = new int[intList.Count];
+        int index = 0;
+        foreach(int number in intList)
+        {
+            output[index] = number;
+            index++;
+        }
+        return output;
+    }
+
+    public static void Dig(object[] array)
+    {
+        foreach(object thing in array)
+        {
+            try
+            {
+                int number = (int)thing;
+                intList.Add(number);
+            }
+            catch
+            {
+                Dig((object[])thing);
+            }
+        }
+    }
+}
+
+
---
+++ Tests/ArrayFlattenerTest.cs
@@ -0,0 +1,20 @@
+﻿using Program;
+
+namespace Tests;
+
+public class ArrayFlattenerTest
+{
+    [Fact]
+    public static void ArrayFlattener_FlattenArray_ReturnIntArray()
+    {
+        object[] arraytest = new object[]
+        {435, 2028, new object[] { new object[]{3047, 4910, 8146, 7999, 1433, 7211, 1197, 6002}, 2821, 3508}};
+
+        int[] result = ArrayFlattener.FlattenArray(arraytest);
+        int[] expected = {435, 2028, 3047, 4910, 8146, 7999, 1433, 7211, 1197, 6002, 2821, 3508};
+
+
+        Assert.Equal(expected,result);
+    }
+    
+}
